5aa404ec7ca996bf2f1f7f4446b70f12f0c5210b
JosephGarrone <josephgarrone@gmail.com>
2016-02-28 21:21:45 +1000

    Commit database

--- database/2016028 - Initial.sql
+++ database/2016028 - Initial.sql
@@ -1,16 +1,10 @@
+CREATE USER 'uq_parking'@'localhost' IDENTIFIED BY 'uq_parking';
+GRANT ALL PRIVILEGES ON uq_parking.* TO 'uq_parking'@'localhost' WITH GRANT OPTION;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `uq_parking` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `uq_parking`;
-
-DROP TABLE IF EXISTS `car_parks`;
-CREATE TABLE IF NOT EXISTS `car_parks` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `data_name` text NOT NULL,
-  `casual` tinyint(1) NOT NULL DEFAULT '1',
-  `active` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `car_park_info`;
 CREATE TABLE IF NOT EXISTS `car_park_info` (
@@ -20,6 +14,14 @@ CREATE TABLE IF NOT EXISTS `car_park_info` (
   `time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `car_parks`;
+CREATE TABLE IF NOT EXISTS `car_parks` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `data_name` text NOT NULL,
+  `casual` tinyint(1) NOT NULL DEFAULT '1',
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `car_parks`
   ADD PRIMARY KEY (`id`);
