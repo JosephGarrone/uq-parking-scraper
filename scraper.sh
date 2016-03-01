02166205c56ba8358c966da4d7569b7f30093fe6
JosephGarrone <josephgarrone@gmail.com>
2016-03-01 22:55:29 +1000

    Commit start/stop script

---
+++ scraper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+DIR="/var/log/uq-parking/"
+FOREVER_LOG="service.log"
+STDOUT="log.log"
+STDERR="log.log"
+PID="pid"
+
+mkdir -p /var/log/uq-parking
+
+if [ $1 = "stop" ]; then
+    forever stop index.js
+else
+    forever start index.js -l $DIR$FOREVER_LOG -o $DIR$STDOUT -e $DIR$STDERR --pidFile $DIR$PID
+fi
+
+
